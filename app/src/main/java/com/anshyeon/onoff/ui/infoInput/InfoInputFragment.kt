c9b76bf4c09ca1b5c87422341d9b5dbc77fcfb1a
ANSHyeon <hyeonan1220@gmail.com>
2023-10-24 01:36:27 +0900

    feat: 로그인 정보 입력 화면 레이아웃 및navigation graph 구현

---
+++ app/src/main/java/com/anshyeon/onoff/ui/infoInput/InfoInputFragment.kt
@@ -0,0 +1,7 @@
+package com.anshyeon.onoff.ui.infoInput
+
+import com.anshyeon.onoff.R
+import com.anshyeon.onoff.databinding.FragmentHomeBinding
+import com.anshyeon.onoff.ui.BaseFragment
+
+class InfoInputFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_info_input)
