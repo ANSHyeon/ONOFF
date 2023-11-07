d76cb388087676bc6d7bbab27d3ef6728b32055c
ANSHyeon <hyeonan1220@gmail.com>
2023-11-07 15:39:27 +0900

    feat: 채팅,게시판,마이페이지 임시 Fragment 구현

---
+++ app/src/main/java/com/anshyeon/onoff/ui/user/UserFragment.kt
@@ -0,0 +1,9 @@
+package com.anshyeon.onoff.ui.user
+
+import com.anshyeon.onoff.R
+import com.anshyeon.onoff.databinding.FragmentHomeBinding
+import com.anshyeon.onoff.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class UserFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_user)
