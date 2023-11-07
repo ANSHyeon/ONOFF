157039813648bdda01a59d6cbb76acb95981df0a
ANSHyeon <hyeonan1220@gmail.com>
2023-11-07 15:40:10 +0900

    feat: Bottom Navigation 구현

--- app/src/main/java/com/anshyeon/onoff/ui/MainActivity.kt
+++ app/src/main/java/com/anshyeon/onoff/ui/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.anshyeon.onoff.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.anshyeon.onoff.R
 import com.anshyeon.onoff.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,5 +19,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.cv_fragment)?.findNavController()
+        navController?.let {
+            setBottomNavigation(it)
+        }
+    }
+
+    private fun setBottomNavigation(navController: NavController) {
+        with(binding.bottomNavigation) {
+            setupWithNavController(navController)
+            setOnItemReselectedListener {}
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                if (destination.id == R.id.navigation_home) {
+                    visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
