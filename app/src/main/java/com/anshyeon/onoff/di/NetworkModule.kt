34fabc0426db496fb3d037a7ac6744cf1ac58cbd
ANSHyeon <hyeonan1220@gmail.com>
2023-11-09 15:13:18 +0900

    feat: NetworkModule kakaoLocal 추가

--- app/src/main/java/com/anshyeon/onoff/di/NetworkModule.kt
+++ app/src/main/java/com/anshyeon/onoff/di/NetworkModule.kt
@@ -3,6 +3,7 @@ package com.anshyeon.onoff.di
 import com.anshyeon.onoff.BuildConfig
 import com.anshyeon.onoff.network.ApiCallAdapterFactory
 import com.anshyeon.onoff.network.FireBaseApiClient
+import com.anshyeon.onoff.network.KakaoLocalApiClient
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
@@ -13,7 +14,16 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Qualifier
 import javax.inject.Singleton
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class FireBaseRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class KakaoLocalRetrofit
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -39,6 +49,7 @@ object NetworkModule {
             .build()
     }
 
+    @FireBaseRetrofit
     @Singleton
     @Provides
     fun provideFireBaseRetrofit(client: OkHttpClient, moshi: Moshi): Retrofit {
@@ -52,7 +63,25 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideFireBaseClient(retrofit: Retrofit): FireBaseApiClient {
+    fun provideFireBaseClient(@FireBaseRetrofit retrofit: Retrofit): FireBaseApiClient {
         return retrofit.create(FireBaseApiClient::class.java)
+    }
+
+    @KakaoLocalRetrofit
+    @Singleton
+    @Provides
+    fun provideKakaoLocalRetrofit(client: OkHttpClient, moshi: Moshi): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.KAKAO_LOCAL_URL)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addCallAdapterFactory(ApiCallAdapterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideKakaoLocalClient(@KakaoLocalRetrofit retrofit: Retrofit): KakaoLocalApiClient {
+        return retrofit.create(KakaoLocalApiClient::class.java)
     }
 }
