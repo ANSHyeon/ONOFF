291a4ca556488b79adfed3d989e69f98e656839c
ANSHyeon <hyeonan1220@gmail.com>
2023-11-21 15:41:01 +0900

    feat: 채팅목록 화면 레이아웃 구현

--- app/src/main/java/com/anshyeon/onoff/data/model/ChatRoom.kt
+++ app/src/main/java/com/anshyeon/onoff/data/model/ChatRoom.kt
@@ -5,6 +5,7 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class ChatRoom(
     val chatRoomId: String,
+    val buildingName: String,
     val latitude: String,
     val longitude: String,
 )
