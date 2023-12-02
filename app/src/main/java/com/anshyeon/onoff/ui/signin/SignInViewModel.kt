16eb101c5018499dbf351eeb239dce11474763e6
ANSHyeon <hyeonan1220@gmail.com>
2023-12-03 02:26:55 +0900

    feat: 로그인 시 유저 정보 유무 판별 로직 수정

--- app/src/main/java/com/anshyeon/onoff/ui/signin/SignInViewModel.kt
+++ app/src/main/java/com/anshyeon/onoff/ui/signin/SignInViewModel.kt
@@ -35,7 +35,7 @@ class SignInViewModel @Inject constructor(private val repository: AuthRepository
             _isLoading.value = true
             val result = repository.getUser()
             result.onSuccess {
-                _hasUserInfo.value = true
+                _hasUserInfo.value = it.values.isNotEmpty()
             }.onError { code, message ->
                 _hasUserInfo.value = false
                 _isLoading.value = false
