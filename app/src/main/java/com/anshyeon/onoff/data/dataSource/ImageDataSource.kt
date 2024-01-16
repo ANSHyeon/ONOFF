1900110264278677826ec1b30a8ce8f5f3e0e667
ANSHyeon <hyeonan1220@gmail.com>
2024-01-16 15:02:48 +0900

    feat: 여러장의 이미지 업로드 비동기 처리 구현

--- app/src/main/java/com/anshyeon/onoff/data/dataSource/ImageDataSource.kt
+++ app/src/main/java/com/anshyeon/onoff/data/dataSource/ImageDataSource.kt
@@ -3,6 +3,8 @@ package com.anshyeon.onoff.data.dataSource
 import android.net.Uri
 import com.anshyeon.onoff.data.model.ImageContent
 import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
@@ -18,9 +20,12 @@ class ImageDataSource @Inject constructor() {
     }
 
     suspend fun uploadImages(imageList: List<ImageContent>): List<String> = coroutineScope {
-        imageList.map { image ->
-            uploadImage(image.uri)
+        val uploadImagesJob = imageList.map { image ->
+            async {
+                uploadImage(image.uri)
+            }
         }
+        uploadImagesJob.awaitAll()
     }
 
     suspend fun downloadImage(location: String): String {
